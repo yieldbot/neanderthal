19b3ea1f10cbdfd88c3dbf1bfa5fdff9750acd14
Mick Thomure <mthomure@gmail.com>
2016-06-22 00:54:49 -0700

    add scalar/vector mul and div helpers

--- test/uncomplicate/neanderthal/native/ops_test.clj
+++ test/uncomplicate/neanderthal/native/ops_test.clj
@@ -3,7 +3,8 @@
             [uncomplicate.neanderthal.core :as c]
             [uncomplicate.neanderthal.native :as n :refer [dv]]
             [uncomplicate.neanderthal.native.ops :refer :all]
-            [uncomplicate.neanderthal.native.util :refer :all]))
+            [uncomplicate.neanderthal.native.util :refer :all]
+            [uncomplicate.neanderthal.native.test-util :refer :all]))
 
 (facts "addition"
   (let [x (dv (range 10))
@@ -64,6 +65,56 @@
     x2 => (dv (range 1 -9 -1))
     (sub! x3 y) => (dv (repeat 10 -10))
     x3 => (dv (repeat 10 -10))
+    y => (dv (range 10 20))))
+
+(facts "multiplication"
+  (let [x (dv (range 10))
+        y (dv (range 10 20))]
+    (mul x 2) => (dv (range 0 20 2))
+    x => (dv (range 10))
+    (mul 2 x) => (dv (range 0 20 2))
+    x => (dv (range 10))
+    (mul x y) => (dv [0 11 24 39 56 75 96 119 144 171])
+    x => (dv (range 10))
+    y => (dv (range 10 20))))
+
+(facts "destructive multiplication"
+  (let [x1 (dv (range 10))
+        x2 (dv (range 10))
+        x3 (dv (range 10))
+        y (dv (range 10 20))]
+    (mul! x1 2) => (dv (range 0 20 2))
+    x1 => (dv (range 0 20 2))
+    (mul! 2 x2) => (dv (range 0 20 2))
+    x2 => (dv (range 0 20 2))
+    (mul! x3 y) => (dv [0 11 24 39 56 75 96 119 144 171])
+    x3 => (dv [0 11 24 39 56 75 96 119 144 171])
+    y => (dv (range 10 20))))
+
+(facts "division"
+  (let [x (dv (range 1 11))
+        y (dv (range 10 20))]
+    (div x 2) => (dv (range 1/2 11/2 1/2))
+    x => (dv (range 1 11))
+    (cmp (div 2 x) (dv [2 1 2/3 1/2 2/5 1/3 2/7 1/4 2/9 1/5])) => true
+    x => (dv (range 1 11))
+    (cmp (div x y)
+         (dv [1/10 2/11 1/4 4/13 5/14 2/5 7/16 8/17 1/2 10/19])) => true
+    x => (dv (range 1 11))
+    y => (dv (range 10 20))))
+
+(facts "destructive division"
+  (let [x1 (dv (range 1 11))
+        x2 (dv (range 1 11))
+        x3 (dv (range 1 11))
+        y (dv (range 10 20))]
+    (div! x1 2) => (dv (range 1/2 11/2 1/2))
+    x1 => (dv (range 1/2 11/2 1/2))
+    (cmp (div! 2 x2) (dv [2 1 2/3 1/2 2/5 1/3 2/7 1/4 2/9 1/5])) => true
+    (cmp x2 (dv [2 1 2/3 1/2 2/5 1/3 2/7 1/4 2/9 1/5])) => true
+    (cmp (div! x3 y)
+         (dv (dv [1/10 2/11 1/4 4/13 5/14 2/5 7/16 8/17 1/2 10/19]))) => true
+    (cmp x3 (dv [1/10 2/11 1/4 4/13 5/14 2/5 7/16 8/17 1/2 10/19])) => true
     y => (dv (range 10 20))))
 
 (facts "the mean of a vector"
