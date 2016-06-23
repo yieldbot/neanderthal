df7a8f5109d01678133b14f79f1caf23f1dc38d3
Mick Thomure <mthomure@gmail.com>
2016-06-22 23:03:58 -0700

    add lstsq solver

--- src/clojure/uncomplicate/neanderthal/native/lapack.clj
+++ src/clojure/uncomplicate/neanderthal/native/lapack.clj
@@ -1,11 +1,13 @@
 (ns uncomplicate.neanderthal.native.lapack
   (:import [uncomplicate.neanderthal LAPACK]
-           [uncomplicate.neanderthal.protocols Block])
+           [uncomplicate.neanderthal.protocols Block]
+           [java.nio ByteBuffer])
   (:require [uncomplicate.neanderthal.core :as c]
             [uncomplicate.neanderthal.native :as n]
             [uncomplicate.neanderthal.real :refer [entry]]
             [uncomplicate.neanderthal.block :refer [buffer order]]
-            [uncomplicate.neanderthal.protocols :refer [COLUMN_MAJOR]]))
+            [uncomplicate.neanderthal.protocols :refer [COLUMN_MAJOR]]
+            [vertigo.bytes :refer [direct-buffer]]))
 
 ;;;;; Low-level LAPACK Routines ;;;;;
 
@@ -193,11 +195,109 @@
   ([^Block a]
    (dgesvd \A \A a)))
 
+(defn -dgelsd
+  ([m n nrhs a b s rcond ^ByteBuffer rank work lwork iwork]
+   ;; NOTE: rank is a bytebuffer holding a single int. is there a cleaner way to
+   ;; return multiple values (in this case, rank and info)?
+   (let [lda (c/mrows a)
+         ldb (c/mrows b)]
+     ;; M >= 0.
+     (assert (pos? m 0))
+     ;; N >= 0.
+     (assert (pos? n 0))
+     ;; NRHS >= 0.
+     (assert (pos? nrhs))
+     ;; A is DOUBLE PRECISION array, dimension (LDA,N)
+     (assert (= (c/ncols a) n))
+     ;; LDA >= max(1,M)
+     (assert (>= lda (max m 1)))
+     ;; B is DOUBLE PRECISION array, dimension (LDB,NRHS)
+     (assert (= (c/ncols b) nrhs))
+     ;; LDB >= max(1,max(M,N))
+     (assert (>= ldb (max 1 m n)))
+     ;; S is DOUBLE PRECISION array, dimension (min(M,N))
+     (assert (= (c/dim s) (min m n)))
+     (assert (every? #(= (order %) COLUMN_MAJOR) [a b])
+             "Matrices must have COLUMN_MAJOR order")
+     (let [info (LAPACK/dgelsd_ m
+                                n
+                                nrhs
+                                (buffer a)
+                                lda
+                                (buffer b)
+                                ldb
+                                (buffer s)
+                                rcond
+                                rank
+                                (buffer work)
+                                lwork
+                                (buffer iwork))
+           rank-val (.getInt rank 0)]
+       (cond
+         (pos? info) (throw (RuntimeException.
+                             "Algorithm failed to converge"))
+         (neg? info) (throw (RuntimeException.
+                             "Unexpected error due to invalid LAPACK argument"))
+         :else {:info info :rank rank-val}))))
+  ([m n nrhs a b s rcond work lwork iwork]
+   (let [rank (direct-buffer Integer/BYTES)]
+     (-dgelsd m n nrhs a b s rcond rank work lwork iwork))))
+
+(defn dgelsd-query [m n nrhs a b s rcond iwork]
+  (let [work0 (n/dv 1)]
+    (-dgelsd m n nrhs a b s rcond work0 -1 iwork)
+    (int (entry work0 0))))
+
+(defn log2 [x]
+  (/ (Math/log x) (Math/log 2)))
+
+(defn dgelsd
+  ([m n nrhs a b s rcond work iwork]
+   (let [{:keys [rank]} (-dgelsd m n nrhs a b s rcond work (c/dim work) iwork)]
+     {:rank rank :x b :s s}))
+  ([m n nrhs a b s rcond]
+   ;; IWORK is INTEGER array, dimension (MAX(1,LIWORK))
+   ;; LIWORK >= max(1, 3 * MINMN * NLVL + 11 * MINMN),
+   ;; where MINMN = MIN( M,N ).
+   ;; NLVL = MAX(0, int( log_2( MINMN / (SMLSIZ+1) ) ) + 1)
+   ;; and SMLSIZ = 25
+   (let [minmn (min m n)
+         nlvl (max 0 (inc (int (log2 (/ minmn 26)))))
+         liwork (+ (* 3 minmn nlvl) (* 11 minmn))
+         iwork (n/dv (max 1 liwork))
+         lwork (dgelsd-query m n nrhs a b s rcond iwork)
+         work (n/dv lwork)]
+     (dgelsd m n nrhs a b s rcond work iwork)))
+  ([a b rcond]
+   (let [m (c/mrows a)
+         n (c/ncols a)
+         nrhs (c/ncols b)
+         s (n/dv (min m n))]
+     (dgelsd m n nrhs a b s rcond)))
+  ([a b]
+   ;; NOTE: b is destroyed!
+   (let [rcond -1]  ;; use machine precision
+     (dgelsd a b -1))))
+
 ;;;;; High-level API ;;;;;
 
-(defn svd [a & {:keys [full-matrices]}]
+(defn svd
+  "Singular-value decomposition of a. Result contains {:s S :u U :vt Vt}."
+  [a & {:keys [full-matrices]}]
   (let [jobz (if full-matrices \A \S)
         a (c/copy a)]  ;; operation is destructive in A
     (dgesdd jobz a)))
+
+(defn lstsq
+  "Least-squares solution to linear system AX = B. If B is a matrix, this is
+  equivalent to solving for each column independently. Result contains
+  {:s S :x X :rank r}, where r and S are the effective rank and singular values
+  of A.
+
+  The effective rank of A is determined by treating as zero those singular
+  values which are less than rcond times the largest singular value."
+  [a b]
+  (let [b (c/copy b)]  ;; operation is destructive in B
+    (dgelsd a b)))
 
 ;; TODO: add macros for creating IFn$DD and $Fn$DDD
